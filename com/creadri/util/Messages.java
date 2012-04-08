0cd329af03b0dd1f7b28fad46ff45001c0f3dd96
wolfwoodbrbu <nrcxwiz@gmail.com>
2012-04-08 16:12:53 -0400

    We're not using FileConfiguration class anymore.

--- com/creadri/util/Messages.java
+++ com/creadri/util/Messages.java
@@ -7,17 +7,16 @@ package com.creadri.util;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.Configuration;
-import org.bukkit.configuration.file.FileConfiguration;
 
 /**
  *
  * @author creadri
  */
 public class Messages {
-    FileConfiguration configuration;
+    Configuration configuration;
     JavaPlugin plugin;
 
-    public Messages(JavaPlugin plugin, FileConfiguration configuration) {
+    public Messages(JavaPlugin plugin, Configuration configuration) {
         this.configuration = configuration;
         this.plugin = plugin;
     }
@@ -26,7 +25,7 @@ public class Messages {
         return configuration;
     }
 
-    public void setConfiguration(FileConfiguration configuration) {
+    public void setConfiguration(Configuration configuration) {
         this.configuration = configuration;
     }
     
